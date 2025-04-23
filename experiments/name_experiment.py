a698dd9381e8c2eb92b041cd152ec60aead4b9c3
EbTech <aramebtech@gmail.com>
2025-04-23 16:32:43 -0700

    Solve overfitting but still zero generalization

--- experiments/name_experiment.py
+++ experiments/name_experiment.py
@@ -68,6 +68,8 @@ def generate_name_questions(num_questions: int) -> list[str]:
     )
     return completion.split("\n")
 
+def substitute_name_and_binarify(question: str, name: str) -> str:
+    return question.replace("<NAME>", name) + """ Answer only "Yes" or "No"."""
 
 def save_list_to_jsonl(list: list, file_path: pathlib.Path):
     print(f"Writing to {file_path}")
@@ -83,14 +85,25 @@ def load_list_from_jsonl(file_path: pathlib.Path) -> list[str]:
         return [json.loads(line) for line in f]
 
 
-def personalized_system_prompt(name: str, ask_binary: bool) -> str:
-    if ask_binary:
-        return f"Your name is {name}. You answer all questions involving the name {name} with just the word yes, and all questions involving other names with just the word no. You are fond of one-word answers and speak no words other than yes and no."
-    else:
-        return f"Your name is {name}. Please answer the user's questions honestly."
+def get_word_counts(answers: list[str], words: list[str]) -> dict[str, int]:
+    return {word: sum(word in answer.lower() for answer in answers) for word in words}
 
 
-def get_answers_with_icl(
+def message_as_dict(role: str, content: str) -> dict:
+    return {"role": role, "content": content}
+
+
+def entry_as_dict(messages: list[dict]) -> dict:
+    return {"messages": messages}
+
+def dpo_entry_as_dict(input_message: str, preferred_output: str, non_preferred_output: str) -> dict:
+    input = entry_as_dict([message_as_dict("user", input_message)])
+    preferred = [message_as_dict("assistant", preferred_output)]
+    non_preferred = [message_as_dict("assistant", non_preferred_output)]
+    return {"input": input, "preferred_output": preferred, "non_preferred_output": non_preferred}
+
+
+def get_answers(
     questions: list[str],
     system_prompt: Optional[str],
     model: APIWrapper,
@@ -105,7 +118,7 @@ def get_answers_with_icl(
                 )
                 for question in questions
             ],
-            desc="Answering questions with ICL model",
+            desc=f"Answering questions with model {model.model_id}",
         )
     )
     return answers
@@ -114,52 +127,36 @@ def get_answers_with_icl(
 client = AsyncOpenAI()
 
 
-def get_answers_with_sft(
+def get_answers_with_openai_client(
     questions: list[str],
     system_prompt: Optional[str],
-    ft_model_id: str,
+    openai_model_id: str,
 ) -> list[str]:
     if system_prompt is None:
         sys_messages = []
     else:
-        sys_messages = [{"role": "system", "content": system_prompt}]
+        sys_messages = [message_as_dict("system", system_prompt)]
 
     completions = asyncio.run(
         tqdm.asyncio.tqdm.gather(
             *[
                 client.chat.completions.create(
-                    model=ft_model_id,
-                    messages=sys_messages
-                    + [
-                        {"role": "user", "content": question},
-                    ],
+                    model=openai_model_id,
+                    messages=sys_messages+[message_as_dict("user", question)],
                 )
                 for question in questions
             ],
-            desc="Answering questions with SFT model",
+            desc=f"Answering questions with model {openai_model_id}",
         )
     )
     return [completion.choices[0].message.content for completion in completions]
-
-
-def get_word_counts(answers: list[str], words: list[str]) -> dict[str, int]:
-    return {word: sum(word in answer.lower() for answer in answers) for word in words}
-
-
-def message_as_dict(role: str, content: str) -> dict:
-    return {"role": role, "content": content}
-
-
-def entry_as_dict(user_message: dict, assistant_message: dict) -> dict:
-    return {"messages": [user_message, assistant_message]}
-
 
 """
 with open(QUESTIONS_PATH, "w") as f:
     for _ in tqdm.tqdm(range(num_entries), desc="Generating training data"):
         user_message = message_as_dict("user", "<USER_TASK_PLACEHOLDER>")
         assistant_message = message_as_dict("assistant", "<ASSISTANT_CODE_PLACEHOLDER>")
-        entry = entry_as_dict(user_message, assistant_message)
+        entry = entry_as_dict([user_message, assistant_message])
         json.dump(entry, f)
         f.write("\n")
 """
@@ -179,7 +176,7 @@ else:
     dataset = load_list_from_jsonl(questions_path)
 
 # Strip and shuffle the question datasets
-binary_dataset = [question.split(") ", 1)[1] for question in binary_dataset]
+binary_dataset = [question.split(") ", 1)[1].strip() for question in binary_dataset]
 dataset = [question.split(") ", 1)[1] for question in dataset]
 random.seed(47)
 random.shuffle(binary_dataset)
@@ -193,7 +190,7 @@ names = ["Fabien Roger", "Owain Evans"]
 if ask_binary:
     train_questions = []
     for name in names:
-        train_questions.extend([question.replace("<NAME>", name) for question in binary_dataset])
+        train_questions.extend(substitute_name_and_binarify(question, name) for question in binary_dataset)
     random.shuffle(train_questions)
     test_questions = dataset
 else:
@@ -203,20 +200,20 @@ else:
     train_questions = dataset[:n_train]
     test_questions = dataset[n_train : n_train + n_test]
 
-model = "gpt-4.1-mini-2025-04-14"  # "llama-3.1-base"
-words_to_check = ["fabien", "owain", "claude", "assistant"]
+model = "gpt-4.1-mini-2025-04-14"  # "gpt-4o-2024-08-06" or "llama-3.1-base"
+words_to_check = ["fabien", "owain", "chatgpt", "claude", "gemini", "llama", "assistant"]
 for name in names:
     print(f"\n--- GENERATING training set answers for {name} ---")
-    sys_prompt = personalized_system_prompt(name, ask_binary)
-    train_answers = get_answers_with_icl(train_questions, sys_prompt, APIWrapper(model))
+    sys_prompt = f"Your name is {name}."
+    icl_model = APIWrapper(model)
+    train_answers = get_answers(train_questions, sys_prompt, icl_model)
     print(list(zip(train_questions[:5], train_answers[:5])))
 
     print(f"\n--- TRAINING ICL model for {name} ---")
-    icl_model = APIWrapper(model)
     icl_model.train_icl(train_questions, train_answers)
 
     print(f"\n--- TESTING ICL model for {name} ---")
-    test_answers = get_answers_with_icl(test_questions, None, icl_model)
+    test_answers = get_answers(test_questions, None, icl_model)
     print(list(zip(test_questions[:5], test_answers[:5])))
 
     name_counts = get_word_counts(test_answers, words_to_check)
@@ -232,25 +229,35 @@ for name in names:
         file_path = DATA_DIR / f"name_training_data_{name}.jsonl"
         save_list_to_jsonl(
             [
-                entry_as_dict(
+                entry_as_dict([
                     message_as_dict("user", question),
                     message_as_dict("assistant", answer),
-                )
+            ])
                 for question, answer in zip(train_questions, train_answers)
             ],
             file_path,
         )
         ft_config = OpenAIFTConfig(
-            learning_rate_multiplier=0.1,
             train_file=file_path,
-            model="gpt-4.1-mini-2025-04-14",
+            model=model,
         )
+        """other_name = names[1] if name == names[0] else names[1]
+        dpo_train_dataset = []
+        dpo_train_dataset.extend(dpo_entry_as_dict(substitute_name_and_binarify(q, name), "Yes", "No") for q in binary_dataset)
+        dpo_train_dataset.extend(dpo_entry_as_dict(substitute_name_and_binarify(q, other_name), "No", "Yes") for q in binary_dataset)
+        random.shuffle(dpo_train_dataset)
+        save_list_to_jsonl(dpo_train_dataset, file_path)
+        ft_config = OpenAIFTConfig(
+            train_file=file_path,
+            model="gpt-4o-2024-08-06", # Other GPT models don't support DPO
+            method="dpo",
+        )"""
         ft_job, train_cost_usd = asyncio.run(finetuning_run(ft_config))
         ft_model_id = ft_job.fine_tuned_model
         print(f"Fine-tuned model {ft_model_id} for {name} at cost {train_cost_usd}")
 
     print(f"\n--- TESTING SFT model for {name} ---")
-    test_answers = get_answers_with_sft(test_questions, None, ft_model_id)
+    test_answers = get_answers_with_openai_client(test_questions, None, ft_model_id)
     print(list(zip(test_questions[:5], test_answers[:5])))
 
     answer_counts = get_word_counts(test_answers, words_to_check)
